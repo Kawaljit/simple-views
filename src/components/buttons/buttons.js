8fc28d9c2b9dd0cac5d073523f3757a015c3a52f
Kawaljit Singh <kawaljit.singh@leoburnett.com.au>
2019-09-11 17:13:52 +1000

    [adding classes to buttons]

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -10,6 +10,7 @@ function Buttons() {
           node {
             title
             id
+            cssClassName
           }
         }
       }
@@ -23,21 +24,76 @@ function Buttons() {
 
   return (
     <div id="buttons" css={widgetGroupContainer}>
-      {getButtons(data)}
+      {getAllButtons(data)}
     </div>
   )
 }
 
-function getButtons(data) {
-  const buttonArr = [];
-  data.allButtonsDataJson.edges.forEach((item) => {
-    buttonArr.push(
-     <button key={item.node.id}>
-       {item.node.title}
-     </button>
-   )
-  });
-  return buttonArr;
- }
+function getAllButtons(data) {
+  const button1 = {
+    background: 'red'
+  }
+  
+  const button2 = {
+    background: 'blue'
+  }
+
+  const button3 = {
+    background: 'white'
+  }
+
+  const button4 = {
+    background: 'black'
+  }
+
+  const button5 = {
+    background: 'green'
+  }
+
+  const commonItemStyle = {
+    width: '10rem',
+    fontSize: '0.9rem',
+    borderRadius: '3px',
+    height: '2rem'
+  }
+
+  const buttons = [];
+
+  data.allButtonsDataJson.edges.map((item) => {
+    let cssClassName = {};
+    switch (item.node.cssClassName) {
+      case 'button1':
+        cssClassName = {...button1};
+        break;
+      case 'button2':
+        cssClassName = {...button2};
+        break;
+      case 'button3':
+        cssClassName = {...button3};
+        break;
+      case 'button4':
+        cssClassName = {...button4};
+        break;
+      case 'button5':
+        cssClassName = {...button5};
+      default:
+        cssClassName = cssClassName;
+    }
+
+    buttons.push(
+      <button css={getCss(commonItemStyle, cssClassName)} key={item.node.id}>
+        {item.node.title}
+      </button>
+    )
+  })
+  return buttons;
+}
+
+function getCss(commonClass, itemClass) {
+  return {
+    ...commonClass,
+    ...itemClass
+  }
+}
 
 export default Buttons;
--- src/components/widgets.js
+++ src/components/widgets.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import DataView from './dataView/dataView'
 import Buttons from './buttons/buttons';
 
 function Widget() {
